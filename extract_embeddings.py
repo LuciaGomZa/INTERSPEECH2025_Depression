cbf1f2e192c66a3c5b54d8433fb16c233fcb5443
Lucía Gómez Zaragozá <lugoza@upv.edu.es>
2025-05-27 10:37:47 +0200

    Update code

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import re
 import logging
 from pathlib import Path
@@ -12,7 +12,7 @@ from transformers import AutoFeatureExtractor, AutoModel, AutoTokenizer
 
 # For delays information in DAIC, see: 
 # M. Hu, L. Liu, X. Wang, Y. Tang, J. Yang, and N. An, “Parallel multiscale bridge fusion network for audio–visual automatic depression assessment,” IEEE Transactions on Computational Social Systems, vol. 11, no. 5, pp. 6830–6842, 2024
-DEFAULT_DELAYS_DAIC = {'300': 35.7, '318': 33.8199, '321': 3.3379, '341': 5.6892, '362': 16.3582}
+DEFAULT_DELAYS_DAIC = {'318': 33.8199, '321': 3.3379, '341': 5.6892, '362': 16.3582, '300': 35.7}
 
 def get_speech_embedding(
     audio: np.ndarray,
